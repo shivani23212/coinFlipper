74e08c54ddb7e315436a6c94ff87719e587c89c6
shivani23212 <65727348+shivani23212@users.noreply.github.com>
2021-06-22 20:15:28 +0100

    Version 2
    Added images, buttons with Javascript event handlers and cleaned up design of webpage.

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 const button = document.getElementById("flip-btn");
-const coin = document.querySelector(".coin");
+const coin = document.querySelector("#coin");
 const resetBtn = document.getElementById("reset-btn");
 
+var heads = 0;
+var tails = 0;
+
+function deferFn(callback, ms) { // callbacks are related to async functions - forces callback function to be called after current function
+    setTimeout(callback, ms); // forces animation to run - even twice in a row
+  }
+  
 
     button.addEventListener("click", function() {
+        coin.setAttribute("class", "");
         const random = Math.random();
         if (random < 0.5) {
-            coin.setAttribute("class", "coin animate-heads");
+            deferFn(function() {
+                coin.setAttribute('class',  "coin animate-heads");
+                heads++;
+                setTimeout(function(){
+                        document.querySelector(".left-text").innerHTML = "Heads: "+heads;
+                        }, 3000);
+            }, 100);
         }
         else {
-            coin.setAttribute("class", "coin animate-tails");
+            deferFn(function() {
+                    coin.setAttribute('class',  "coin animate-tails");
+                    tails++;
+                    setTimeout(function(){
+                            document.querySelector(".right-text").innerHTML = "Tails: "+tails;
+                            }, 3000);
+                }, 100);
         }
     });
 
 
+
     resetBtn.addEventListener("click", function() {
-        coin.setAttribute("class", "coin");        
+        coin.setAttribute("class", "coin"); 
+        heads = 0;
+        tails = 0;
+        document.querySelector(".left-text").innerHTML = "Heads: "+heads;
+        document.querySelector(".right-text").innerHTML = "Tails: "+tails;
     });
